24eec138e2d0775c390492f51a8372fd538a147b
Cui Weijie <cwjcw@sohu.com>
2025-05-03 17:53:05 +0800

    111

--- spiders/xhs.py
+++ spiders/xhs.py
@@ -8,7 +8,7 @@ from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
